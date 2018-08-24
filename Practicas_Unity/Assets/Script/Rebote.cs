e35448a63be195543d179979729f7c67d3e7b635
Efren Mario Gonzalez Lopez <marioxbox3601@live.com.mx>
2018-08-24 00:07:02 -0700

    Se agrego fisicas al juego y se esta tratando de poner fisica en un personaje descargado o hacer que camine

---
+++ Practicas_Unity/Assets/Script/Rebote.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+//using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rebote : MonoBehaviour {
+
+	
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("La pelota colisiono con"+ collision.gameObject.name);
+        //Debug.log("La pelota choco con" + collision.gameObject.GetComponent.name);
+    }
+
+}
