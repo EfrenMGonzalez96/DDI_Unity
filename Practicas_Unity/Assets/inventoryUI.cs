122f8893e7cfccbe286f42025d4c6bc39f375e70
Efren Mario Gonzalez Lopez <marioxbox3601@live.com.mx>
2018-09-26 20:24:14 -0700

    ok

---
+++ Practicas_Unity/Assets/inventoryUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryUI : MonoBehaviour {
+    Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
